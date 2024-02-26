94ee2853594f59553c60a5f94b3642c493585c03
enrico <noreply@enricoe.de>
2024-02-26 18:43:57 +0100

    Added function to run suspend functions

--- src/main/kotlin/de/hglabor/mongo/MongoUtils.kt
+++ src/main/kotlin/de/hglabor/mongo/MongoUtils.kt
@@ -2,11 +2,15 @@ package de.hglabor.mongo
 
 import de.hglabor.mongo.config.MongoConfigFile
 import de.hglabor.mongo.database.MongoConnection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import java.io.File
 
 object MongoUtils {
     var database: CoroutineDatabase? = null
+    var coroutineScope = CoroutineScope(Dispatchers.IO)
 
     /**
      * Establishes a connection to the MongoDB database using the provided configuration file
@@ -18,5 +22,19 @@ object MongoUtils {
         val credentials = mongoConfig.read()
         database = MongoConnection.establish(credentials)
         return database
+    }
+}
+
+/**
+ * Asynchronously executes the provided suspendable [runnable] function within a coroutine scope.
+ *
+ * @param runnable The suspendable function to be executed asynchronously.
+ *
+ * @see kotlinx.coroutines.launch
+ * @see MongoUtils.coroutineScope
+ */
+fun async(runnable: suspend () -> Unit) {
+    MongoUtils.coroutineScope.launch {
+        runnable()
     }
 }
