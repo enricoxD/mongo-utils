8c158fbf4c36844beb30ac88ca6718a2e9705c3a
enrico <71661047+enricoxD@users.noreply.github.com>
2024-09-07 13:44:17 +0200

    Update UUID Serializer

--- src/main/kotlin/de/hglabor/mongo/serialization/UUIDSerializer.kt
+++ src/main/kotlin/de/hglabor/mongo/serialization/UUIDSerializer.kt
@@ -1,20 +1,30 @@
 package de.hglabor.mongo.serialization
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.ByteArraySerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.nio.ByteBuffer
 import java.util.*
 
-object  UUIDSerializer : KSerializer<UUID> {
-    override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): UUID {
-        return UUID.fromString(decoder.decodeString())
-    }
+object UUIDSerializer : KSerializer<UUID> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("de.hglabor.mongo.serialization.UUIDSerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: UUID) {
-        encoder.encodeString(value.toString())
+        val byteBuffer = ByteBuffer.allocate(16)
+        byteBuffer.putLong(value.mostSignificantBits)
+        byteBuffer.putLong(value.leastSignificantBits)
+        encoder.encodeSerializableValue(ByteArraySerializer(), byteBuffer.array())
+    }
+
+    override fun deserialize(decoder: Decoder): UUID {
+        val byteArray = decoder.decodeSerializableValue(ByteArraySerializer())
+        val byteBuffer = ByteBuffer.wrap(byteArray)
+        val mostSigBits = byteBuffer.long
+        val leastSigBits = byteBuffer.long
+        return UUID(mostSigBits, leastSigBits)
     }
 }
